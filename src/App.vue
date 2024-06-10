7abf3ca93a7d5e2cf329b12f18a08cd8f8e3a3c2
AngeloC89 <Angelociulla89@gmail.com>
2024-06-10 18:37:01 +0200

    created a function with axios for get the projects and print on DOM

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,38 @@
 <template>
   <h1 class="text-center">Ciao</h1>
+  <ul >
+    <li v-for="project in projects" :key="project.id">{{ project.title }}</li>
+  
+  </ul>
   <i class="fa fa-solid fa-home"></i>
 </template>
 
 <script>
+  import { store } from './store.js';
+  import axios from 'axios';
   export default {
-    name: 'App'
+    name: 'App',
+    data() {
+      return {
+        store,
+        projects: []
+      }
+    },
+    methods: {
+      getAllProjects() {
+        axios
+          .get(this.store.apiBaseUrl + '/projects')
+          .then((res) => {
+            this.projects = res.data.results
+
+            //console.log(res.data)
+          })
+      }
+    },
+    mounted() {
+      this.getAllProjects()
+    },
   }
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
